e8a7e089df1b8dcadd7c798139d4f17098bfd7cd
arunravichandran-11 <arunravichandran11@gmail.com>
2021-11-15 00:13:43 +0530

    mockdata export error fix to root

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT;
 
-var whitelist = [
-  "https://wayfair-transportation-fe-53a645.netlify.app/",
-  "http://localhost:3000/",
-];
+// var whitelist = [
+//   "https://wayfair-transportation-fe-53a645.netlify.app/",
+//   "http://localhost:3000/",
+// ];
 
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to node app");
