73708e8e264be259b4dbd602337c968eac867afb
arunravichandran-11 <arunravichandran11@gmail.com>
2021-11-14 18:03:02 +0530

    Simple nodeapp to deploy in netlify

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { NavigationData } from "./mockData/navigation";
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.get("/nav-menu", (req, res) => {
+  res.send(NavigationData);
+});
+
+app.listen(port, () => {
+  // console.log(`server started at ${port}`);
+});
