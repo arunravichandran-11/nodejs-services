e6762cc88951522bbb89e5351d4ddaee951e436d
arunravichandran-11 <arunravichandran11@gmail.com>
2021-11-14 23:24:11 +0530

    Port is dynamically set with process object

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const mock = require("./src/mockData/navigation");
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
   res.send("Welcome to node app");
 });
 app.get("/nav-menus", (req, res) => {
   res.send(mock);
 });
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log("app Start");
 });
