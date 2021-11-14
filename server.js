ec10a9556e814416441c9b46a47a8ad5a81b82b3
arunravichandran-11 <arunravichandran11@gmail.com>
2021-11-14 23:55:16 +0530

    cors enabled for fe-challenge wayfaie

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const mock = require("./src/mockData/navigation");
 const app = express();
-
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
+
+var whitelist = [
+  "https://wayfair-transportation-fe-53a645.netlify.app/",
+  "http://localhost:3000/",
+];
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Welcome to node app");
